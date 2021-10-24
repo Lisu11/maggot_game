0c70d74a7389cc3ac6f837edf1e8b78606ba0549
Piotrek Lisowski <piotrek.lisowski11@gmail.com>
2021-10-24 04:54:04 +0200

    small fixes and gamesr list added

--- apps/maggot_web/lib/maggot_web/live/game_live.ex
+++ apps/maggot_web/lib/maggot_web/live/game_live.ex
@@ -8,14 +8,19 @@ defmodule MaggotWeb.GameLive do
     Logger.debug(:mount_socket)
     topic = "room:" <> room
     username =  UUID.uuid1() # replace it and get user from session
-    if connected?(socket) do
-      Endpoint.subscribe(topic)
-      Presence.track(self(), topic, username, %{})
-    end
+    gamers =
+        if connected?(socket) do
+          Endpoint.subscribe(topic)
+          Presence.track(self(), topic, username, %{})
+          Presence.list(topic) |> Map.keys() |> MapSet.new()
+        else
+          MapSet.new()
+        end
     {:ok, assign(socket,
             topic: topic,
             messages: [],
-            username: username)}
+            username: username,
+            gamers: gamers)}
   end
 
   @impl true
@@ -36,24 +41,37 @@ defmodule MaggotWeb.GameLive do
 
   @impl true
   def handle_info(%{event: "presence_diff", payload: payload}, socket) do
-    broadcast_presence(payload, socket.assigns.topic)
-    {:noreply, socket}
+    {:noreply,
+      socket
+       |> update_presence(payload)
+       |> update_gamers(payload)}
   end
 
-  defp broadcast_presence(payload, topic) do
-    Enum.each(payload, fn {status, diffs} ->
-      for msg <- diffs_to_messages(diffs, status) do
-        Endpoint.broadcast(topic, "new-message", msg)
-      end
-    end)
+  defp update_gamers(socket, %{joins: j, leaves: l}) do
+    assign(socket,
+      gamers:
+        Map.keys(j)
+          |> MapSet.new()
+          |> MapSet.union(socket.assigns.gamers)
+          |> MapSet.difference(MapSet.new(Map.keys(l))))
   end
+
+  defp update_presence(socket, payload) do
+    [j_msgs, l_msgs] =
+      for {status, diffs} <- payload do
+         diffs_to_messages(diffs, status)
+      end
+    assign(socket, messages: j_msgs ++ l_msgs)
+  end
+
   defp diffs_to_messages(diff, status) do
     diff
       |> Map.keys()
       |> Enum.map(fn u ->
-          %{id: UUID.uuid4(),
-            txt: status_on_message(u, status),
-            type: :system}
+           %{id: UUID.uuid4(),
+             txt: status_on_message(u, status),
+             type: :system,
+             user: u}
         end)
   end
   defp status_on_message(username, :joins) do
