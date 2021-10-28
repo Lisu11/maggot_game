4d7b77ec2ad0a729fdd8ffe3ab9d73332741e9e5
Piotrek Lisowski <piotrek.lisowski11@gmail.com>
2021-10-28 06:25:15 +0200

    chat moved to seperate live_component

--- apps/maggot_web/lib/maggot_web/live/chat_live.ex
+++ apps/maggot_web/lib/maggot_web/live/chat_live.ex
@@ -3,18 +3,150 @@ defmodule MaggotWeb.ChatLive do
   require Logger
   alias MaggotWeb.Endpoint
 
+
+  @impl true
   def mount(socket) do
-    IO.inspect("------------------------")
-    IO.inspect(socket)
-    {:ok, socket}
+    # IO.inspect(socket)
+    {:ok, assign(socket, :gamers, nil)}
   end
 
   @impl true
-  def handle_event("send-message", %{"input" => %{"message" => message}}, socket) do
-    Logger.info(socket)
+  def update(assigns, socket) do
+    socket
+    #  |> IO.inspect()
+     |> assign(assigns)
+     |> assign_gamers(assigns.initial_presence)
+    #  |> IO.inspect()
+     |> parse_message(assigns.raw_message, assigns.current_user)
+    #  |> IO.inspect()
+     |> parse_diff(assigns.presence_diff)
+    #  |> IO.inspect()
+     |> Kernel.then(fn s -> {:ok, s} end)
+  end
+
+  @impl true
+  def handle_event("send-message", %{"input" => %{"message" => message, "send_to" => to}}, socket) do
+    Logger.info(send_message: message)
+    message =
+      %{id: UUID.uuid4(),
+        txt: message,
+        type: regular_or_private(to),
+        user: socket.assigns.current_user.username}
     Endpoint.broadcast(socket.assigns.topic, "new-message", message)
+
     {:noreply, socket}
   end
 
+  defp regular_or_private(to_who) do
+    if to_who == "" do
+      :regular
+    else
+      {:private, to_who}
+    end
+  end
+
+  defp parse_message(socket, %{type: {:private, to}} = msg, user) do
+    messages = if user.username == to, do: [msg], else: []
+    assign(socket, messages: messages)
+  end
+  defp parse_message(socket, msg, _user) do
+    if msg, do: assign(socket, messages: [msg]), else: socket
+  end
+
+  defp parse_diff(socket, nil), do: socket
+  defp parse_diff(socket, diff) do
+    socket
+      |> update_presence(diff)
+      |> update_gamers(diff)
+  end
+
+  def gamers_select_options(gamers) do
+    for gamer <- gamers do
+      gamer
+    end
+  end
+
+  def render_message(%{type: {:private, to}} = msg, current_user, assigns \\ %{}) do
+    Logger.debug(maybe_rendering_private_message: msg)
+    if current_user.username == to do
+      ~L"""
+      <li id="<%= msg.id %>" class="list-group-item">
+        <strong class="username">
+            <%= msg.user %>
+        </strong> whispers:
+        <div class="message-content text-danger">
+          <%= msg.txt %>
+        </div>
+      </li>
+      """
+    end
+  end
+  def render_message(%{type: :system} = msg, _current_user, assigns) do
+    Logger.debug(rendering_system_message: msg)
+    ~L"""
+    <li id="<%= msg.id %>" class="list-group-item text-info">
+        <em class="message-content system-message">
+            @<strong class="username"><%= msg.user %></strong>
+            <%= msg.txt %>
+        </em>
+    </li>
+    """
+  end
+  def render_message(msg, _current_user, assigns) do
+    Logger.debug(rendering_regular_message: msg)
+    ~L"""
+    <li id="<%= msg.id %>" class="list-group-item">
+      <strong class="username"><%= msg.user %></strong> said:
+      <div class="message-content">
+        <%= msg.txt %>
+      </div>
+    </li>
+    """
+  end
+
+  defp assign_gamers(%{assigns: %{gamers: nil}} = socket, init) do
+    assign(socket, :gamers, init)
+  end
+  defp assign_gamers(socket, _), do: socket
+
+  defp update_gamers(socket, %{joins: j, leaves: l}) do
+    assign(socket,
+      gamers:
+        Map.keys(j)
+          # |> IO.inspect()
+          |> MapSet.new()
+          |> MapSet.union(socket.assigns.gamers)
+          |> MapSet.difference(MapSet.new(Map.keys(l))))
+  end
+
+  defp update_presence(socket, payload) do
+    messages = socket.assigns.messages
+    [j_msgs, l_msgs] =
+      for {status, diffs} <- payload do
+         diffs_to_messages(diffs, status)
+      end
+    send(self(), :presence_diff_consumed)
+    assign(socket,
+      messages: messages ++ j_msgs ++ l_msgs,
+      presence_diff: nil)
+  end
+
+  defp diffs_to_messages(diff, status) do
+    diff
+      |> Map.keys()
+      # |> IO.inspect()
+      |> Enum.map(fn u ->
+           %{id: UUID.uuid4(),
+             txt: status_on_message(status),
+             type: :system,
+             user: u}
+        end)
+  end
+  defp status_on_message(:joins) do
+    "has joined the chat"
+  end
+  defp status_on_message(:leaves) do
+    "has left the chat"
+  end
 
 end
--- apps/maggot_web/lib/maggot_web/live/game_live.ex
+++ apps/maggot_web/lib/maggot_web/live/game_live.ex
@@ -8,7 +8,7 @@ defmodule MaggotWeb.GameLive do
     Logger.debug(:mount_socket)
     topic = "room:" <> room
     socket = assign_user(socket, token)
-    gamers =
+    initial_presence =
         if connected?(socket) do
           Endpoint.subscribe(topic)
           Presence.track(self(), topic, socket.assigns.current_user.username, %{})
@@ -18,8 +18,10 @@ defmodule MaggotWeb.GameLive do
         end
     {:ok, assign(socket,
             topic: topic,
+            raw_message: nil,
             messages: [],
-            gamers: gamers)}
+            presence_diff: nil,
+            initial_presence: initial_presence)}
   end
 
   defp assign_user(socket, token) do
@@ -28,126 +30,21 @@ defmodule MaggotWeb.GameLive do
     end)
   end
 
-
-  def render_message(%{type: {:private, to}} = msg, current_user, assigns \\ %{}) do
-    Logger.debug(maybe_rendering_private_message: msg)
-    if current_user.username == to do
-      ~L"""
-      <li id="<%= msg.id %>" class="list-group-item">
-        <strong class="username">
-            <%= msg.user %>
-        </strong> whispers:
-        <div class="message-content text-danger">
-          <%= msg.txt %>
-        </div>
-      </li>
-      """
-    end
-  end
-  def render_message(%{type: :system} = msg, _current_user, assigns) do
-    Logger.debug(rendering_system_message: msg)
-    ~L"""
-    <li id="<%= msg.id %>" class="list-group-item text-info">
-        <em class="message-content system-message">
-            @<strong class="username"><%= msg.user %></strong>
-            <%= msg.txt %>
-        </em>
-    </li>
-    """
-  end
-  def render_message(msg, _current_user, assigns) do
-    Logger.debug(rendering_regular_message: msg)
-    ~L"""
-    <li id="<%= msg.id %>" class="list-group-item">
-      <strong class="username"><%= msg.user %></strong> said:
-      <div class="message-content">
-        <%= msg.txt %>
-      </div>
-    </li>
-    """
-  end
-
-  @impl true
-  def handle_event("send-message", %{"input" => %{"message" => message, "send_to" => to}}, socket) do
-    Logger.info(send_message: message)
-    message =
-      %{id: UUID.uuid4(),
-        txt: message,
-        type: regular_or_private(to),
-        user: socket.assigns.current_user.username}
-    Endpoint.broadcast(socket.assigns.topic, "new-message", message)
-
-    {:noreply, socket}
-  end
-
-  defp regular_or_private(to_who) do
-    if to_who == "" do
-      :regular
-    else
-      {:private, to_who}
-    end
-  end
-
   @impl true
   def handle_info(%{event: "new-message", payload: message}, socket) do
     Logger.info(handle_info: message)
 
-    { :noreply, assign_message(socket, message)}
+    { :noreply, assign(socket, :raw_message, message)}
   end
   @impl true
   def handle_info(%{event: "presence_diff", payload: payload}, socket) do
-    {:noreply,
-      socket
-       |> update_presence(payload)
-       |> update_gamers(payload)}
+    {:noreply, assign(socket, :presence_diff, payload)}
+  end
+  @impl true
+  def handle_info(:presence_diff_consumed, socket) do
+    Logger.debug(:presence_diff_consumed)
+    {:noreply, assign(socket, :presence_diff, nil)}
   end
 
-  defp assign_message(socket, %{type: {:private, to}} = msg) do
-    messages = if socket.assigns.current_user.username == to, do: [msg], else: []
-    assign(socket, messages: messages)
-  end
-  defp assign_message(socket, msg), do: assign(socket, messages: [msg])
 
-
-  def gamers_select_options(gamers) do
-    for gamer <- gamers do
-      gamer
-    end
-  end
-
-  defp update_gamers(socket, %{joins: j, leaves: l}) do
-    assign(socket,
-      gamers:
-        Map.keys(j)
-          # |> IO.inspect()
-          |> MapSet.new()
-          |> MapSet.union(socket.assigns.gamers)
-          |> MapSet.difference(MapSet.new(Map.keys(l))))
-  end
-
-  defp update_presence(socket, payload) do
-    [j_msgs, l_msgs] =
-      for {status, diffs} <- payload do
-         diffs_to_messages(diffs, status)
-      end
-    assign(socket, messages: j_msgs ++ l_msgs)
-  end
-
-  defp diffs_to_messages(diff, status) do
-    diff
-      |> Map.keys()
-      # |> IO.inspect()
-      |> Enum.map(fn u ->
-           %{id: UUID.uuid4(),
-             txt: status_on_message(status),
-             type: :system,
-             user: u}
-        end)
-  end
-  defp status_on_message(:joins) do
-    "has joined the chat"
-  end
-  defp status_on_message(:leaves) do
-    "has left the chat"
-  end
 end
