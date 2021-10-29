c61f2c453bd5c84a81c266e4b18b63da1baec978
Piotrek Lisowski <piotrek.lisowski11@gmail.com>
2021-10-29 03:47:48 +0200

    small Chnages.ex refactor

--- apps/maggot_engine/lib/game/changes.ex
+++ apps/maggot_engine/lib/game/changes.ex
@@ -1,7 +1,7 @@
 defmodule MaggotEngine.Game.Changes do
 
   def new(pluses, minuses), do: %{"+" => pluses, "-" => minuses}
-  def empty_changes, do: %{"+" => [], "-" => []}
+  def empty_changes, do: new([], [])
 
   def merge_changes(l, r) do
     Map.merge(l, r, fn _k, v1, v2 -> v1 ++ v2 end)
